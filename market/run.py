b30d23fd898b7633bc667d293cb139a1f9b9e330
lennartvandeguchte <lvandeguchte@hotmail.nl>
2017-10-11 21:10:31 +0200

    Kan nu trainen en testen op simulated data

--- market/run.py
+++ market/run.py
@@ -1,25 +1,40 @@
 from model import *
 import matplotlib.pyplot as plt
 import numpy
+import json
+import pickle
 
 # run.py
 #all_wealth = []
-#all_history = []
+amountSteps = 415
+amountRuns = 3
 all_history = numpy.zeros(415)
+all_history_save = {}
 
 #amount of simulations
-for j in range(1):
+for j in range(amountRuns):
     # create new model for a simulation
     model = Market()
 
     #max range (timesteps) equals 415 (830 days)
-    for i in range(415):
+    for i in range(amountSteps):
         model.step()
         all_history[i] += model.globalPriceHistory[i]
     #show results for simulation j after 415 timesteps
+    all_history_save['results%s' % j]= model.globalPriceHistory
 
-for h in range(415):
-    all_history[h] = all_history[h]/100
+
+with open('data/simulationPrices.pkl', 'wb') as f:
+    pickle.dump(all_history_save, f, pickle.HIGHEST_PROTOCOL)
+
+
+
+#learning agent learns
+
+
+
+for h in range(amountSteps):
+    all_history[h] = all_history[h]/amountRuns
 plt.plot(all_history)
 plt.show()
 
