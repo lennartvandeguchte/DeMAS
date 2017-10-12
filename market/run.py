f5b10a34d093e30c443a643d3dc588ad9e593cfc
lennartvandeguchte <lvandeguchte@hotmail.nl>
2017-10-12 14:56:38 +0200

    Working learning, still needs give the right figures

--- market/run.py
+++ market/run.py
@@ -16,7 +16,7 @@ for j in range(amountRuns):
     # create new model for a simulation
     model = Market()
 
-    #max range (timesteps) equals 415 (830 days)
+    #max range (timesteps) equals 415 (830 days), this simulates the period between 01-01-2012 and 10-04-2014
     for i in range(amountSteps):
         model.step()
         all_history[i] += model.globalPriceHistory[i]
@@ -24,27 +24,29 @@ for j in range(amountRuns):
     all_history_save['results%s' % j]= model.globalPriceHistory
 
 
+# Save simulated data to train and test the learning agent
 with open('data/simulationPrices.pkl', 'wb') as f:
     pickle.dump(all_history_save, f, pickle.HIGHEST_PROTOCOL)
 
 
 
-#learning agent learns
+# Learning agent trains and tests
+# Can be commented and runned seperately to use the same simulated data, useful for parameter optimalization
+exec(open("trainLearningModel.py").read()) 
 
 
-
+# Show the average simulated bitcoin price over an amount of runs
 for h in range(amountSteps):
     all_history[h] = all_history[h]/amountRuns
 plt.plot(all_history)
 plt.show()
 
-    #plt.plot(model.globalPriceHistory)
+   
+
+
+# Store the results
+    #for agent in model.schedule.agents:
+    #    all_wealth.append(agent.wealth)
+
+    #plt.hist(all_wealth, bins=range(max(all_wealth)+1))
     #plt.show()
-
-
-    # Store the results
-        #for agent in model.schedule.agents:
-        #    all_wealth.append(agent.wealth)
-
-        #plt.hist(all_wealth, bins=range(max(all_wealth)+1))
-        #plt.show()
